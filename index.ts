95577c04da6e0a1d6d5d038b04f5f589ecb370f3
236sec <popole159za@gmail.com>
2023-03-02 19:32:31 +0700

    Add all the file to project

--- index.ts
+++ index.ts
@@ -4,15 +4,29 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-let map: google.maps.Map;
+let map: google.maps.Map, infoWindow: google.maps.InfoWindow;
 
 function initMap(): void {
-  map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+  const map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+    center: { lat: 13.848285, lng: 100.569100 },
+    zoom: 18,
+    mapId: '517913eb51add9e4'
   });
+
+  const BusStopImg = document.createElement('img');
+  BusStopImg.src = 'https://i.postimg.cc/7h6M1ct0/icons8-bus-24.png';
+  const BusStop1 = new google.maps.marker.AdvancedMarkerView({
+    map,
+    position: { lat: 13.848285, lng: 100.569100 },
+    content: BusStopImg,
+    title: 'Bus1'
+});
+
 }
 
+let pos = {lat: 13.848285 ,
+  lng: 100.569100 ,};
+  
 declare global {
   interface Window {
     initMap: () => void;
@@ -20,3 +34,4 @@ declare global {
 }
 window.initMap = initMap;
 export {};
+
